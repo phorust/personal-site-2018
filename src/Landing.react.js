460a846ad3743cb42c6f1956f17b0cdac4b31adb
Kevin Lee <kevinmlee.x@gmail.com>
2018-08-29 17:36:42 -0700

    prevent right clicking

--- src/Landing.react.js
+++ src/Landing.react.js
@@ -3,7 +3,7 @@ import JAPAN_VIDEO from './static/00136_4.mp4';
 
 const Landing = () => (
   <div className="page black">
-    <video autoPlay loop>
+    <video autoPlay loop onContextMenu={e => e.preventDefault()}>
       <source src={JAPAN_VIDEO} type="video/mp4" />
     </video>
   </div>
