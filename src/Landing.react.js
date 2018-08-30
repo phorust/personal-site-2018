f3b4bc0b271b3727426427aa4b7c1c1fa3111077
Kevin Lee <kevinmlee.x@gmail.com>
2018-08-29 18:44:21 -0700

    host on s3

--- src/Landing.react.js
+++ src/Landing.react.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import JAPAN_VIDEO from './static/00136_4.mp4';
 
 const Landing = () => (
   <div className="page black">
     <video autoPlay loop onContextMenu={e => e.preventDefault()}>
-      <source src={JAPAN_VIDEO} type="video/mp4" />
+      <source
+        src="https://s3-us-west-2.amazonaws.com/mngyuan-com/00136_4.mp4"
+        type="video/mp4"
+      />
     </video>
   </div>
 );
