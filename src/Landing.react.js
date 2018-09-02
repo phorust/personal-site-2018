64203c198f1cd732dc52568436ec7ef6a3cea780
Kevin Lee <kevinmlee.x@gmail.com>
2018-09-02 01:43:21 -0700

    add playsinline to video

--- src/Landing.react.js
+++ src/Landing.react.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Landing = () => (
   <div className="page black">
-    <video autoPlay loop onContextMenu={e => e.preventDefault()}>
+    <video playsinline autoPlay muted onContextMenu={e => e.preventDefault()}>
       <source
         src="https://s3-us-west-2.amazonaws.com/mngyuan-com/00136_4.mp4"
         type="video/mp4"
