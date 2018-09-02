8e346955b84612bc6a15643e4881d9d44b7f91e4
Kevin Lee <kevinmlee.x@gmail.com>
2018-09-02 02:13:54 -0700

    add photo of me to about

--- src/About.react.js
+++ src/About.react.js
@@ -1,55 +1,61 @@
 import React from 'react';
+import ME_PHOTO from './photos2/me alameda.jpg';
 
 const About = () => (
-  <div className="theOne">
-    <div className="theOneInner">
-      <span>
-        Kevin writes code. He takes photos and makes layouts and the occasional
-        graphic.
+  <React.Fragment>
+    <div className="mePhoto">
+      <img src={ME_PHOTO} />
+    </div>
+    <div className="theOne">
+      <div className="theOneInner">
+        <span>
+          Kevin writes code. He takes photos and makes layouts and the
+          occasional graphic.
+          <br />
+          <br />
+          He listens to podcasts and adores video essays and indie games, and
+          has lots of opinions.
+          <br />
+          <br />
+          He is looking for the next thing he wants to create.
+          <br />
+          <br />
+          <a
+            href="http://instagram.com/mngyuan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @mngyuan
+          </a>
+        </span>
         <br />
         <br />
-        He listens to podcasts and adores video essays and indie games, and has
-        lots of opinions.
-        <br />
-        <br />
-        He is looking for the next thing he wants to create.
-        <br />
+        <div className="line" />
         <br />
         <a
-          href="http://instagram.com/mngyuan"
+          href="http://dribbble.com/phorust"
           target="_blank"
           rel="noopener noreferrer"
         >
-          @mngyuan
+          dribbble
         </a>
-      </span>
-      <br />
-      <br />
-      <div className="line" />
-      <br />
-      <a
-        href="http://dribbble.com/phorust"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        dribbble
-      </a>
-      <a
-        href="https://twitter.com/kevinlee324"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        twitter
-      </a>
-      <a
-        href="http://github.com/phorust/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        github
-      </a>
+        <a
+          href="https://twitter.com/kevinlee324"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          twitter
+        </a>
+        <a
+          href="http://github.com/phorust/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>
+      </div>
     </div>
-  </div>
+  </React.Fragment>
 );
 
 export default About;
