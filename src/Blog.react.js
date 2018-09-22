3906aeb8a720ebb71b878669aadf7ea512d70bae
Kevin Lee <kevinmlee.x@gmail.com>
2018-09-21 17:10:37 -0700

    finish blog post draft

--- src/Blog.react.js
+++ src/Blog.react.js
@@ -1,33 +1,188 @@
 import React from 'react';
 import BLURB_PHOTO from './photos2/ice cream/L9997552.jpg';
-import COVER_PHOTO_1 from './photos2/ice cream/L9997517.jpg';
-import COVER_PHOTO_2 from './photos2/ice cream/L9997541.jpg';
-import COVER_PHOTO_3 from './photos2/ice cream/L9997560.jpg';
+import COVER_PHOTO from './photos2/ice cream/L9997560.jpg';
+import PHOTO_1 from './photos2/ice cream/L9997541.jpg';
+import PHOTO_2 from './photos2/blog/tam/L9997509.jpg';
+import PHOTO_3 from './photos2/blog/tam/108891AAle072307-R1-032.jpg';
+import PHOTO_4 from './photos2/blog/tam/108891AAle072307-R1-005.jpg';
+import PHOTO_5 from './photos2/blog/tam/108891AAle072307-R1-002.jpg';
+import PHOTO_6 from './photos2/ice cream/L9997645.jpg';
+import PHOTO_7 from './photos2/blog/tam/108891AAle072312-R1-014.jpg';
+import PHOTO_8 from './photos2/blog/tam/108891AAle072312-R1-013.jpg';
 
 const Blog = () => (
-  <React.Fragment>
-    <div className="blogEntry">
-      <div className="coverImages">
-        <img className="coverImage" src={COVER_PHOTO_1} />
-        <img className="coverImage two" src={COVER_PHOTO_2} />
-        <img className="coverImage three" src={COVER_PHOTO_3} />
+  <div className="blogEntry">
+    <div
+      className="photo"
+      style={{
+        marginLeft: '27vw',
+      }}
+    >
+      <img src={COVER_PHOTO} />
+      <div
+        className="columnsText"
+        style={{
+          left: '30%',
+          top: '40%',
+        }}
+      >
+        <p>
+          waiting on the sun to set.
+          <br />
+          <br />
+          it's not my first time here, and i'm not sure there'll be a last. i've
+          had multiple people tell me, with some surprise in their voices, that
+          i go here quite often. That it seems like i'm always here.
+          <br />
+          <br />
+          the horizon cracks and gives way to golden, rolling hills, each
+          raising and lowering our car alternately, like lazy waves, churning
+          slowly.
+          <br />
+          <br />
+          can you blame me?
+          <br />
+          <br />
+          <br />
+          <br />
+          sometimes i think about leaving the bay
+        </p>
       </div>
-      <div className="blogBlurb">
-        <h1>Mt Tamalpais</h1>
-        <h4>07.15.17</h4>
-        <br />
-        <div className="line" />
-        <br />
-        <p>アイスクリーム above the clouds,</p>
-        <br />
-        <img src={BLURB_PHOTO} />
-        <br />
-        <br />
-        <p>with friends,</p>
+      <div
+        className="columnsText"
+        style={{
+          left: '60%',
+          top: '40%',
+        }}
+      >
+        <p>and sometimes i think how wrong i'd be to do so.</p>
       </div>
     </div>
-    <div className="blogEntry">test</div>
-  </React.Fragment>
+    <div className="photo">
+      <img src={PHOTO_1} />
+      <div className="flavorText" style={{left: '70%', top: '25%'}}>
+        <h1>the wind eats at me. </h1>
+      </div>
+      <div
+        className="columnsText"
+        style={{
+          left: '110%',
+          top: '40%',
+        }}
+      >
+        <p>
+          between breaths i wonder about nothing. the shutter on my camera fails
+          and comes back to life in the same moment.
+        </p>
+      </div>
+    </div>
+    <div className="photo inset">
+      <img src={PHOTO_2} />
+    </div>
+    <div className="photo inset">
+      <img src={PHOTO_3} />
+      <div
+        className="columnsText"
+        style={{
+          left: '80%',
+          top: '40%',
+        }}
+      >
+        <p>
+          i think if i'd've left this city sooner, i'd've grown up faster.
+          <br /> <br />
+          i'd've had to left some people here, leave some things behind. i'd've
+          had to plant new seeds, and hope they'd grow, and i'd have to check
+          in, and call, just to keep loved ones close, and watch less loved ones
+          go,
+          <br /> <br />
+          the time between calls getting longer, the messages left read getting
+          older.
+        </p>
+      </div>
+    </div>
+    <div className="photo">
+      <img src={PHOTO_4} />
+    </div>
+    <div className="photo" style={{marginLeft: 400}}>
+      <img src={PHOTO_5} />
+      <div
+        className="columnsText"
+        style={{
+          left: '80%',
+          top: '40%',
+        }}
+      >
+        <p>
+          instead, i'm watching them rot up close, from neglect, rather than
+          space.
+        </p>
+      </div>
+    </div>
+    <div className="photo" style={{marginLeft: 600}}>
+      <img src={PHOTO_6} />
+      <div
+        className="columnsText"
+        style={{
+          left: '-20%',
+          top: '40%',
+        }}
+      >
+        <p>
+          you live two blocks from here. it's the fourth time i've been by and
+          not said hello. it's just, i'm busy, and you're busy, even though i
+          think we both have time, just that just because i'd do anything with
+          you,
+          <br /> <br />
+          doesn't mean we will.
+        </p>
+      </div>
+    </div>
+    <div className="photo">
+      <img src={PHOTO_7} />
+    </div>
+    <div className="photo">
+      <img src={PHOTO_8} />
+      <div className="flavorText" style={{left: '70%', top: '25%'}}>
+        <h1>the sun flickers</h1>
+      </div>
+      <div
+        className="columnsText"
+        style={{
+          left: '80%',
+          top: '60%',
+        }}
+      >
+        <p>
+          from beneath the horizon.
+          <br />
+          <br />
+          if i'd've left this city sooner, i think i'd've grown up faster,
+          watching these things change, swirling around me, just new, new, new,
+          fighting for meaning, rootless changes dying to be made sense of.
+          <br />
+          <br />
+          instead, i'm watching myself change up close, placed somewhere between
+          these unmoving hills,
+          <br /> <br />
+          feeling myself grow into them as the people come and go.
+        </p>
+      </div>
+    </div>
+    <div className="blogBlurb">
+      <h1>Mt Tamalpais</h1>
+      <h4>07.15.17</h4>
+      <br />
+      <div className="line" />
+      <br />
+      <p>アイスクリーム above the clouds,</p>
+      <br />
+      <img src={BLURB_PHOTO} />
+      <br />
+      <br />
+      <p>with friends,</p>
+    </div>
+  </div>
 );
 
 export default Blog;
