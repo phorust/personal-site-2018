23adf33816b324e6d2d216ec0621d697a7b477e6
Kevin Lee <kevinmlee.x@gmail.com>
2018-10-02 13:49:05 -0700

    unmute video on click

--- src/Landing.react.js
+++ src/Landing.react.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Landing = () => (
   <div className="page black">
-    <video playsinline autoPlay muted onContextMenu={e => e.preventDefault()}>
+    <video
+      playsinline
+      autoPlay
+      muted
+      onContextMenu={e => e.preventDefault()}
+      onClick={e => (e.target.muted = !e.target.muted)}
+    >
       <source
         src="https://s3-us-west-2.amazonaws.com/mngyuan-com/00136_4.mp4"
         type="video/mp4"
