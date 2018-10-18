03f45ac4bb731e76994ebadad7a8ab52fc4a8339
Kevin Lee <kevinmlee.x@gmail.com>
2018-10-17 22:43:23 -0400

    better meta tags for photo galleries

--- src/Photos.react.js
+++ src/Photos.react.js
@@ -5,62 +5,77 @@ import Helmet from 'react-helmet';
 import {importAll} from 'mngyuan-lib';
 import Topbar from './Topbar.react';
 
-const photos = {
-  contacts: importAll(
-    require.context('./photos/contacts', false, /\.(png|jpe?g|svg)$/),
-  ).reverse(),
-  'kchaooo for oliviashihdesign': importAll(
-    require.context(
-      './photos/kchaooo for oliviashihdesign',
-      false,
-      /\.(png|jpe?g|svg)$/,
+const SETS = {
+  contacts: {
+    photos: importAll(
+      require.context('./photos/contacts', false, /\.(png|jpe?g|svg)$/),
+    ).reverse(),
+    preamble: '',
+  },
+  'kchaooo for oliviashihdesign': {
+    photos: importAll(
+      require.context(
+        './photos/kchaooo for oliviashihdesign',
+        false,
+        /\.(png|jpe?g|svg)$/,
+      ),
     ),
-  ),
-  'thepigs for mcmintea': importAll(
-    require.context(
-      './photos/thepigs for mcmintea',
-      false,
-      /\.(png|jpe?g|svg)$/,
+    preamble: (
+      <div className="preamble" key="preamble">
+        Fuji GA645i<br />
+        Fujicolor Pro 400H<br />
+        <br />
+        with Kelley<br />
+        05.27.18<br />
+      </div>
     ),
-  ),
-  'lumohn in nyc': importAll(
-    require.context('./photos/lumohn in nyc', false, /\.(png|jpe?g|svg)$/),
-  ),
-  travel: importAll(
-    require.context('./photos/travel', false, /\.(png|jpe?g|svg)$/),
-  ),
-  'nyc and antoinette': importAll(
-    require.context('./photos/nyc and antoinette', false, /\.(png|jpe?g|svg)$/),
-  ),
-};
-
-const PREAMBLES = {
-  'kchaooo for oliviashihdesign': (
-    <div className="preamble" key="preamble">
-      Fuji GA645i<br />
-      Fujicolor Pro 400H<br />
-      <br />with Kelley
-      <br />05.27.18
-    </div>
-  ),
-  'thepigs for mcmintea': (
-    <div className="preamble" key="preamble">
-      Canon 5DMk4<br />
-      24-70L<br />
-      <br />
-      <br />with @senaponin, @thepigs, for @mcmintea
-      <br />07.28.18
-    </div>
-  ),
-  'nyc and antoinette': (
-    <div className="preamble" key="preamble">
-      Leica CL buddies<br />
-      Minolta M-Rokkor 40mm f/2<br />
-      <br />
-      <br />raw scans from Sammy's
-      <br />10.13.18
-    </div>
-  ),
+  },
+  'thepigs for mcmintea': {
+    photos: importAll(
+      require.context(
+        './photos/thepigs for mcmintea',
+        false,
+        /\.(png|jpe?g|svg)$/,
+      ),
+    ),
+    preamble: (
+      <div className="preamble" key="preamble">
+        Canon 5DMk4<br />
+        24-70L<br />
+        <br />
+        with @senaponin, @thepigs, for @mcmintea<br />
+        07.28.18<br />,
+      </div>
+    ),
+  },
+  'lumohn in nyc': {
+    photos: importAll(
+      require.context('./photos/lumohn in nyc', false, /\.(png|jpe?g|svg)$/),
+    ),
+  },
+  travel: {
+    photos: importAll(
+      require.context('./photos/travel', false, /\.(png|jpe?g|svg)$/),
+    ),
+  },
+  'nyc and antoinette': {
+    photos: importAll(
+      require.context(
+        './photos/nyc and antoinette',
+        false,
+        /\.(png|jpe?g|svg)$/,
+      ),
+    ),
+    preamble: (
+      <div className="preamble" key="preamble">
+        Leica CL buddies<br />
+        Minolta M-Rokkor 40mm f/2<br />
+        <br />
+        raw scans from Sammy's <br />
+        10.13.18<br />
+      </div>
+    ),
+  },
 };
 
 export class Gallery extends React.Component {
@@ -94,15 +109,19 @@ export class Gallery extends React.Component {
 }
 
 const Photos = props => {
-  const {match} = props;
-  console.log(match.params.set);
-  const photoElems = photos[match.params.set].map(src => (
-    <img key={src} src={src} />
-  ));
-  const elems = ([PREAMBLES[match.params.set]] || []).concat(photoElems);
+  const {set} = props.match.params;
+  console.log(set);
+  const photoElems = SETS[set].photos.map(src => <img key={src} src={src} />);
+  const elems = ([SETS[set].preamble] || []).concat(photoElems);
   return (
     <React.Fragment>
-      <Helmet title="photo · Kevin Lee" />
+      <Helmet title={`${set} | photo · Kevin Lee`}>
+        <meta property="og:title" content={set} />
+        <meta
+          property="og:image"
+          content={SETS[set].photos[SETS[set].coverPhotoIndex || 0]}
+        />
+      </Helmet>
       <Gallery>{elems}</Gallery>
       <Topbar />
     </React.Fragment>
